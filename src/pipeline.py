77e81a3ae6da2c761d7fddcf2a00071f1ae99101
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-04-05 13:17:44 +0545

    drop target column to separate it from preprocessing

--- src/pipeline.py
+++ src/pipeline.py
@@ -3,21 +3,26 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 import numpy as np
 
-def build_pipeline(df, num_scaler, model_type, params):
-    object_cols = df.select_dtypes(include=['object']).columns.tolist()
-    num_cols = df.select_dtypes(include=[np.number]).columns.tolist()
-    bin_cols = [c for c in object_cols if df[c].nunique() == 2]
-    nom_cols = [c for c in object_cols if df[c].nunique() > 2]
+def build_pipeline(df, num_scaler, model, target_col):
+    features = df.drop(columns=[target_col])
+
+    object_cols = features.select_dtypes(include=['object']).columns.tolist()
+    num_cols = features.select_dtypes(include=[np.number]).columns.tolist()
     
+    bin_cols = [c for c in object_cols if features[c].nunique() == 2]
+    nom_cols = [c for c in object_cols if features[c].nunique() > 2]
+    
+    scaler_to_use = num_scaler if num_scaler is not None else "passthrough"
+
     categorical_encoder = ColumnTransformer([
         ("ordinal", OrdinalEncoder(), bin_cols),
         ("nominal", OneHotEncoder(handle_unknown='ignore'), nom_cols),
-        ("numerical", num_scaler, num_cols)
-    ])
+        ("numerical", scaler_to_use, num_cols)
+    ], remainder='passthrough')
 
     pipeline = Pipeline([
         ("preprocessor",categorical_encoder),
-        ("model", model_type(**params))
+        ("model", model)
     ])
 
     return pipeline
