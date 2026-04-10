549af01c1027fb9db98044a6a16e39cd379fcaab
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-04-10 19:05:38 +0545

    added mapping within the objective function to address storage of classes

--- src/experimentation.py
+++ src/experimentation.py
@@ -5,6 +5,7 @@ import yaml
 import pandas as pd
 from optuna.visualization import plot_pareto_front
 import argparse
+from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler
 
 from src.model_mapper import scale_map
 from src.model_train import train_model
@@ -12,12 +13,14 @@ from src.pipeline import build_pipeline
 from config import EXPERIMENTATION_DIR
 
 def _suggest_param(trial: Trial, name, value):
-    if isinstance(value, list) and len(value) > 2:
+    if isinstance(value, list):
+        if len(value) == 2 and isinstance(value[0], int) and not isinstance(value[0], bool):
+            return trial.suggest_int(name, value[0], value[1])
+
+        if len(value) == 2 and isinstance(value[0], float):
+            return trial.suggest_float(name, value[0], value[1])
+        
         return trial.suggest_categorical(name, value)
-    if isinstance(value, list) and len(value) == 2 and isinstance(value[0], int):
-        return trial.suggest_int(name, value[0], value[1])
-    if isinstance(value, list) and len(value) == 2 and isinstance(value[0], float):
-        return trial.suggest_float(name, value[0], value[1])
     
     return value
 
@@ -30,14 +33,27 @@ def objective(trial: Trial, df, experiment_file, search_grid, target_col):
         model_class = model_cfg["regressor"]
         model_name = model_class.__name__
 
-        scaler_class = trial.suggest_categorical(f"{model_name}_scaler", model_cfg["scalers"])
+        scaler_map = {
+            "StandardScaler": StandardScaler,
+            "RobustScaler": RobustScaler,
+            "MinMaxScaler": MinMaxScaler,
+            "None": None
+        }
+
+        available_scaler_names = [
+            s.__name__ if hasattr(s, '__name__') else "None" 
+            for s in model_cfg["scalers"]
+        ]
+
+        scaler_name = trial.suggest_categorical(f"{model_name}_scaler", available_scaler_names)
+
+        scaler_class = scaler_map[scaler_name]
         num_scaler = scaler_class() if scaler_class is not None else None
 
         params = {}
         for key, value in model_cfg.items():
             if key in ["regressor", "scalers"]:
                 continue
-            
             p_name = f"{model_name}_{key}"
             params[key] = _suggest_param(trial, p_name, value)
 
@@ -72,7 +88,7 @@ def start_experiment(experiment_file_name):
         study = optuna.create_study(directions=["minimize", "minimize"])
         study.optimize(
             lambda trial: objective(trial, df, experiment_file, search_grid, target_col), 
-            n_trials=50
+            n_trials=experiment_file["train"]["n_trials"]
         )
 
         pareto_front = plot_pareto_front(study, target_names=["RMSE", "Latency"])
